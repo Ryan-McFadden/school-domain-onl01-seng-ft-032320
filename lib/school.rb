235df6da90307757ef53020d86c98653920a0cf9
Ryan McFadden <ryanmcfadden123@gmail.com>
2020-04-01 21:03:34 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -5,13 +5,5 @@ class School
     @school_name = school_name
   end
   
-  def roster
-    @roster = {}
-  end
-  
-  def add_student=(name, grade)
-    @roster[grade] = []
-    @roster[grade] << name
-  end
   
 end
