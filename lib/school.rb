a6657707470bd9b9fdb9fe16167d3cc7b5bc1d66
Ryan McFadden <ryanmcfadden123@gmail.com>
2020-04-01 20:30:55 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,16 +1,4 @@
 # code here!
 class School
-  attr_reader :school_name
-  
-  @roster = {}
-  
-  def initialize(school_name)
-    @school_name = school_name
-  end
-  
-  def add_student(name, grade)
-    @roster[name] = []
-    @roster[name] << grade
-  end
   
 end
